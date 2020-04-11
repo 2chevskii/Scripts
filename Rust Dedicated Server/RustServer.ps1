637f95d8df6877e5004f24bb2ba34a63445240bb
2CHEVSKII <prsroman3@gmail.com>
2020-04-11 20:13:34 +0300

    ...

--- Rust Dedicated Server/RustServer.ps1
+++ Rust Dedicated Server/RustServer.ps1
@@ -17,6 +17,9 @@ param (
     [switch]$UpdateUmod,
 
     [Parameter(ParameterSetName = 'NonInteractive')]
+    [switch]$Force,
+
+    [Parameter(ParameterSetName = 'NonInteractive')]
     [switch]$Clean,
 
     [Parameter(ParameterSetName = 'NonInteractive')]
@@ -85,6 +88,8 @@ $umod_api_link = 'https://umod.org/games/rust.json'
 
 $umod_game_lib_name = 'Oxide.Rust.dll' # will be changed in the future with umod release
 
+#region Core
+
 function Install-UMod {
     Write-Colorized "[----] Preparing to update uMod ..."
 
@@ -94,7 +99,8 @@ function Install-UMod {
     } elseif ($IsLinux) {
         $download_link = $umod_dl.develop
     } else {
-        throw 'Your platform is unsupported.'
+        Write-Colorized "[<red>FAIL</red>] Your platform is unsupported."
+        return $false
     }
 
     $managed_folder_path = $settings.GetManagedFolderPath()
@@ -104,8 +110,10 @@ function Install-UMod {
 
     if (!$lib_exists) {
         Write-Colorized "[<red>XXXX</red>] uMod is not installed, installing now ..."
-    } elseif (!(Test-NeedOxideUpdate)) {
+    } elseif (Test-NeedOxideUpdate) {
         Write-Colorized "[<red>XXXX</red>] uMod is outdated, updating ..."
+    } elseif ($Force) {
+        Write-Colorized "[<red>XXXX</red>] Force update enabled, updating ..."
     } else {
         Write-Colorized "[ <green>OK</green> ] uMod update is unnecessary."
         return $true
@@ -122,22 +130,93 @@ function Install-UMod {
         Invoke-WebRequest -Uri $download_link -OutFile $download_path
         Write-Colorized "[ <green>OK</green> ] uMod build downloaded successfully."
     } catch {
-        Write-Colorized "[<red>FAIL</red>] Could not download uMod archive: $_"
+        Write-Colorized "[<red>FAIL</red>] Could not download uMod archive: <red>$_</red>"
         return $false
     } finally {
         $ProgressPreference = 'Continue'
     }
 
-    
+    try {
+        Write-Colorized "[<yellow>WAIT</yellow>] Extracting uMod files ..."
+        Expand-Archive -Path $download_path -DestinationPath = $settings.server_path -Force
+        Write-Colorized "[ <green>OK</green> ] Files were extracted successfully."
+    } catch {
+        Write-Colorized "[<red>FAIL</red>] Could not extract uMod files from archive: <red>$_</red>"
+        return $false
+    }
+
+    Write-Colorized "[ <green>OK</green> ] uMod was successfully updated."
+    return $true
 }
+
+function Install-Server {
+    Write-Colorized "[----] Preparing for server update ..."
+    $installation_path = Resolve-PathNoFail $settings.server_path
+    $steamcmd_script_path = Resolve-PathNoFail $settings.steamcmd_script_path
+    $steamcmd_installation_path = Resolve-PathNoFail $settings.steamcmd_installation_path
+    Write-Colorized "[----] Server installation folder: <blue>$installation_path</blue>"
+    Write-Colorized "[----] SteamcmdHelper script path: <blue>$steamcmd_script_path</blue>"
+    Write-Colorized "[----] Steamcmd installation path: <blue>$steamcmd_installation_path</blue>"
+
+    if (!(Test-Path $steamcmd_script_path)) {
+        Write-Colorized "[<red>FAIL</red>] Could not find steamcmdHelper script, make sure it's located at: $steamcmd_script_path"
+        return $false
+    }
+
+    try {
+        Write-Colorized "[<yellow>WAIT</yellow>] Updating server files ..."
+        Start-Process -FilePath $steamcmd_script_path -ArgumentList "-AppID $appID -InstallDir $installation_path -SteamcmdDir $steamcmd_installation_path -Validate !$Force" -NoNewWindow -Wait -ErrorAction Stop -PassThru
+        if ($LASTEXITCODE -ne 0 -and $LASTEXITCODE -ne 7) {
+            throw "Script returned code $LASTEXITCODE."
+        }
+        Write-Colorized "[ <green>OK</green> ] Server updated successfully."
+        return $true
+    } catch {
+        Write-Colorized "[<red>FAIL</red>] Could not update server using SteamcmdHelper: <red>$_</red>"
+        return $false
+    }
+}
+
+#endregion
 
 #region Helpers
 
 function Test-NeedOxideUpdate {
-    param (
+    Write-Colorized "[<yellow>WAIT</yellow>] Checking currently installed uMod version ..."
+    $assembly_path = Join-Path $settings.GetManagedFolderPath() $umod_game_lib_name
 
-    )
+    try {
+        $assembly = [System.Reflection.Assembly]::LoadFrom($assembly_path)
+    } catch {
+        Write-Colorized "[<red>FAIL</red>] Failed to load existing assembly: <red>$_</red>"
+        return $true
+    }
 
+    $current_version = $assembly.GetName().Version
+
+    try {
+        $json = Invoke-WebRequest -Uri $umod_api_link | Select-Object -ExpandProperty Content
+    } catch {
+        Write-Colorized "[<red>FAIL</red>] Failed get data from uMod api: <red>$_</red>"
+        return $true
+    }
+
+    $latest_version = [version]::new(($json | ConvertFrom-Json -AsHashtable).latest_release_version)
+
+    Write-Colorized "[----] Current uMod.Rust version: $current_version"
+    Write-Colorized "[----] Latest uMod.Rust version: $latest_version"
+
+    $comparison_result = $current_version.CompareTo($latest_version)
+
+    if ($comparison_result -eq -1) {
+        Write-Colorized "[<red>XXXX</red>] "
+        return $true
+    } elseif ($comparison_result -eq 0) {
+        Write-Colorized "[ <green>OK</green> ] Versions are equal, update is not necessary."
+    } else {
+        Write-Colorized "[ <green>OK</green> ] Current version is newer than the latest one published. Are you Wulf or what?"
+    }
+    return $false
 }
 
 function Resolve-PathNoFail {
